cf237b9b089e805ed5b29b53f3b7928e33f32dea
David Betancourt <dmarquez.develop@gmail.com>
2025-06-24 01:22:46 +0000

    delete app.js from main and added code to /src/app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronombres = ['the', 'our'];
+  let adjetivos = ['great', 'big'];
+  let sustantivos = ['jogger', 'racoon'];
+  let extensiones = ['.com', '.es'];
+
+  pronombres.forEach(pronombre => {
+      adjetivos.forEach(adjetivo => {
+          sustantivos.forEach(sustantivo => {
+
+              let domain = `${pronombre}${adjetivo}${sustantivo}`;
+
+              extensiones.forEach(extension => {
+                  console.log(`${domain}` + extension);
+              });
+      
+          });
+      });
+  });
   console.log("Hello Rigo from the console!");
 };
